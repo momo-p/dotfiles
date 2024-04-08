9da2b2454da5fb2b0aa4fd5d9c5ae25c4a769ce6
momo-p <contact@relay.akimitsu.dev>
2024-04-08 08:40:41 +0700

    ci: project init

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- Pick a theme
+config.color_scheme = "Material Palenight (base16)"
+config.color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" }
+config.window_background_opacity = 0.92
+
+-- Use Jetbrain Mono font
+config.font = wezterm.font 'JetBrains Mono'
+
+-- Disable tab sinc I don't use it
+config.enable_tab_bar = false
+
+-- Disable bottom padding sinc it break nvim bar
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 2,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ .zshrc
@@ -0,0 +1,12 @@
+autoload -U promptinit; promptinit
+prompt pure
+
+export EDITOR="nvim"
+
+eval "$(zoxide init zsh)"
+
+alias python="python3"
+alias nvm-init="source /usr/share/nvm/init-nvm.sh"
+alias clear="clear; pfetch"
+alias cd="z"
+clear
